87fb276f9c9fc422248ab8ffab43d125018a41b8
Nick <nickxz149@gmail.com>
2019-12-07 13:33:50 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
-import './App.css';
+import Download from '@axetroy/react-download';
 
-function App() {
+const password = (len) => {
+  let chars = ['!', '#', "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+  for (let i = chars.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = chars[i];
+    chars[i] = chars[j];
+    chars[j] = temp;
+  }
+  chars.length = len;
+  let password = chars.join('');
+  return password
+}
+
+const App = () => {
   return (
     <div className="App">
+      <div className="container">
+        <br />
+        <h2>Utils.Xestra.us</h2>
+        <br />
+        <h3>Random Passwords</h3>
+        <p>A simple password generator that creates 3 different password with 3 different lengths.</p>
+        <code>{password(8)}</code>
+        <code>{password(16)}</code>
+        <code>{password(24)}</code><br />
+        <br />
+        <h3>Pastebin</h3>
+        <p>This is a basic pastebin made by hastebin but edited and hosted by Xestra.</p>
+        <a className="btn" href="https://paste.xestra.us" target="_blank">paste.xestra.us</a><br />
+        <br />
+        <h3>Useful Scripts</h3>
+        <p>A collection of useful scripts that anyone can use.</p>
+        <div className="deck">
+          <div className="card">
+            <h4>Lorem Lorem</h4>
+            <p>This card is reserved for future projects. Come back later to see if something new is here!</p>
+            <Download file="test.txt">
+              <a className="btn">Download</a><br />
+            </Download>
+          </div>
+          <div className="card">
+            <h4>Reserved</h4>
+            <p>This card is reserved for future projects. Come back later to see if something new is here!</p>
+            <a className="btn">Download</a><br />
+          </div>
+          <div className="card">
+            <h4>Reserved</h4>
+            <p>This card is reserved for future projects. Come back later to see if something new is here!</p>
+            <a className="btn">Download</a><br />
+          </div>
+          <div className="card">
+            <h4>Reserved</h4>
+            <p>This card is reserved for future projects. Come back later to see if something new is here!</p>
+            <a className="btn">Download</a><br />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './reset.scss';
+import './App.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
